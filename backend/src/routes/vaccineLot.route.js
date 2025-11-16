9782a550a3d5b651fd452a44be2b6a6eb53cbd82
trinhxuanhoa <bongnaiton30@gmail.com>
2025-11-16 20:18:04 +0700

    feat(api): CRUD handlers for vaccines and vaccine_lot

--- backend/src/routes/vaccineLot.route.js
+++ backend/src/routes/vaccineLot.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { totalStock, expiringBatches, deleteLot, addLot} from "../controllers/vaccineLot.controller.js";
+import { totalStock, expiringBatches, deleteLot, editLot, addLot} from "../controllers/vaccineLot.controller.js";
 import {  } from "../controllers/vaccineLot.controller.js";
 const router = express.Router();
 
 router.post("/", addLot);
 router.delete("/:id", deleteLot);
+router.put("/:id", editLot);
 router.get("/stock", protectRoute, totalStock);
 router.get("/expiring", protectRoute, expiringBatches);
 export default router;
