4eee74e15d0b39bf823de46459fd1d0beec11be9
Nguyen Hieu <https://github.com/HieuNguyen1905>
2025-11-17 20:35:21 +0700

    update route for bill details

--- backend/src/routes/administration.route.js
+++ backend/src/routes/administration.route.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   createAdministration,
   searchCitizensByNationalId,
+  getBillDetails,
+  updateBillStatus,
 } from "../controllers/administration.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
@@ -9,5 +11,6 @@ const router = express.Router();
 
 router.put("/:appointmentId", protectRoute, createAdministration);
 router.get("/", protectRoute, searchCitizensByNationalId);
-
+router.get("/:appointmentId/bill", protectRoute, getBillDetails);
+router.put("/:appointmentId/bill", protectRoute, updateBillStatus);
 export default router;
