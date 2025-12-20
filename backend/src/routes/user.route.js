a6c5bebbfe49778178106be2abf3ef75b3c732df
Nguyen Hieu <https://github.com/HieuNguyen1905>
2025-12-20 17:40:25 +0700

    feat(api): getEmployeeProfile, updateEmployeeProfile

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,13 @@
-import { getCitizenProfile, updateCitizenProfile, getVaccineHistory, getNotifications } from "../controllers/user.controller.js";
+import { getCitizenProfile, updateCitizenProfile, getVaccineHistory, getNotifications, getEmployeeProfile, updateEmployeeProfile } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/me", protectRoute, getCitizenProfile);
+router.get("/employee/me", protectRoute, getEmployeeProfile);
 router.put("/me", protectRoute, updateCitizenProfile);
+router.put("/employee/me", protectRoute, updateEmployeeProfile);
 router.get("/me/vaccine-history", protectRoute, getVaccineHistory);
 router.get("/me/notifications", protectRoute, getNotifications);
 export default router;
